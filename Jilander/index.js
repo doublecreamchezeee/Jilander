7d7c8f6d396e09cf2a319ab0dc152bafb435e5a0
21120575 <nguyen.thanhtri2112@gmail.com>
2024-04-03 21:55:27 +0700

    [UI] Config Material Bottm Bar

--- Jilander/index.js
+++ Jilander/index.js
@@ -6,55 +6,96 @@ import {AppRegistry} from 'react-native';
 import DetailTask from './screens/DetailTask';
 import {name as appName} from './app.json';
 import HomeScreen from './screens/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Account from './screens/Account';
+import Setting from './screens/Setting';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const Stack = createNativeStackNavigator();
 const MyTask = () => {
-    return (
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-      >
-        <Stack.Group>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Group>
-        <Stack.Group
-          screenOptions={{ 
-            presentation: 'transparentModal', 
-            headerShown: true, 
-            animation: 'slide_from_bottom'
-          }}
-        >
-          <Stack.Screen name="DetailTask" component={DetailTask} />  
-        </Stack.Group>
-        </Stack.Navigator>
-    );
-  };
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Group>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Group>
+      <Stack.Group
+        screenOptions={{
+          presentation: 'transparentModal',
+          headerShown: true,
+          animation: 'slide_from_bottom',
+        }}>
+        <Stack.Screen name="DetailTask" component={DetailTask} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
 
-  const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
-  const AppContainer = () => {
-    return (
-      <NavigationContainer>
-      <Tab.Navigator 
-      initialRouteName="Home"
-      activeColor="#e91e63"
-      barStyle={{ backgroundColor: 'darkblue' }}
-      >
-        <Tab.Screen 
-          name="MyTask" 
+const AppContainer = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        barStyle={{
+          position: 'absolute',
+          // bottom: 25,
+          left: 20,
+          right: 20,
+          // elevation: 0,
+          backgroundColor: '#ffffff',
+          borderRadius: 20,
+          // height: 70
+        }}>
+        <Tab.Screen
+          name="MyTask"
           component={MyTask}
           options={{
             tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons
+                name="home"
+                color={color}
+                size={26}
+                style={{marginBottom: -3}}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={Account}
+          options={{
+            tabBarLabel: 'Account',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons
+                name="target-account"
+                color={color}
+                size={26}
+                style={{marginBottom: -3}}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Setting"
+          component={Setting}
+          options={{
+            tabBarLabel: 'Setting',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons
+                name="table-settings"
+                color={color}
+                size={26}
+                style={{marginBottom: -3}}
+              />
             ),
           }}
         />
       </Tab.Navigator>
     </NavigationContainer>
-    );
-  };
+  );
+};
 
 AppRegistry.registerComponent(appName, () => AppContainer);
